48b339ada2957b1f795d8bc51e247703dcd4c350
qawi <qawitherev@gmail.com>
2022-08-21 22:14:45 +0800

    added enter anim from navigation *fixed* the toolbar -> still need improvement slight layout fix

--- app/src/main/java/com/abing/ultraman/UltramanListActivity.kt
+++ app/src/main/java/com/abing/ultraman/UltramanListActivity.kt
@@ -3,24 +3,32 @@ package com.abing.ultraman
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
+import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
 
 class UltramanListActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ultraman_list)
 
         setSupportActionBar(findViewById(R.id.ultraman_list_toolbar))
-        getSupportActionBar()!!.setDisplayHomeAsUpEnabled(true)
-        getSupportActionBar()!!.setDisplayShowHomeEnabled(true)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        val navController = navHostFragment.navController
+        navController = navHostFragment.navController
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.ultraman_list_activity_toolbar, menu)
         return true
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
